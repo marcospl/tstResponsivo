84570513ee7a874c1f12841ad4ce7b0ac209c7f3
Marcos PL <marcoswebdev@gmail.com>
2014-07-19 04:52:57 -0300

    Criação de todas as páginas

--- contato.php
+++ contato.php
@@ -9,6 +9,7 @@
 			</span>
 		</div>
 			<h2>Email: <a href="">fulanodetal@projetotst.com</a></h2>
+			<h2>Telefone: <a href="">(11) 0000-0000</a></h2>
 	</article>
 <?php 
 	require "common/footer.php";
